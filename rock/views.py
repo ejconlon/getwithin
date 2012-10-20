e118bfa67647b2612fa25962dd97e70792dbe9d3
Eric Conlon <ejconlon@gmail.com>
2012-10-20 11:14:47 -0700

    set default

--- rock/views.py
+++ rock/views.py
@@ -4,12 +4,20 @@ from django.http import HttpResponse
 
 def base_context_dict():
   navlinks = []
-  navlinks.append(('Home', '/', True))
+  navlinks.append(('Home', '/', False))
   return {'title': 'GetWithin', 'name': 'GetWithin', 'navlinks': navlinks}
+
+def set_current(d, name):
+  d['header'] = name
+  l = d['navlinks']
+  for i in xrange(len(l)):
+    if l[i][0] == name:
+      l[i] = (l[i][0], l[i][1], True)
+      break
 
 def index(request):
     t = get_template('layout.html')
     d = base_context_dict()
-    d['header'] = 'Home'
+    set_current(d, 'Home')
     html = t.render(Context(d))
     return HttpResponse(html)
